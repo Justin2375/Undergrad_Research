21eb2e0e673b9efe2577b8105164ffdfe7aa58f1
Jacob Reed <jacob.reed2017@gmail.com>
2016-10-05 00:12:13 -0500

    workingon adding explain flag

--- SentiTool/src/sentitool/SentiTool.java
+++ SentiTool/src/sentitool/SentiTool.java
@@ -5,19 +5,21 @@
  */
 package sentitool;
 import java.util.Scanner;
+import java.util.*;
 import uk.ac.wlv.sentistrength.*;
 
 /**
  *
- * @author jacob
+ * @author jacob reed
+ * @see http://www.jake-reed.com
  */
 public class SentiTool {
 
     private static final String SENTI_DATA_PATH = "/home/jacob/Development/"
             + "Undergrad_Research/SentiStrength/SentStrength_Data_Sept2011/"; // String contains the path to the SentiData folder
-    private static String mode; // What mode to run as.  Almost always is text.
-    private static boolean explainStat; // Flag if the user wants to have the results explained or not
-    private Scanner in = new Scanner(System.in); // Scanner for taking in user defined vars
+    private static String mode, textOrFile; // What mode to run as.  Almost always is text.
+    private static String explainStat; // Flag if the user wants to have the results explained or not
+    private static Scanner in = new Scanner(System.in); // Scanner for taking in user defined vars
     
     /**
      * @param args the command line arguments
@@ -25,7 +27,19 @@ public class SentiTool {
     public static void main(String[] args) {
     //Method 1: one-off classification (inefficient for multiple classifications)
     //Create an array of command line parameters, including text or file to process
-    String ssthInitialisationAndText[] = {"sentidata", SENTI_DATA_PATH, "text", , "explain"};
+    System.out.println("Enter text to be analyzed.");
+    textOrFile = in.nextLine();
+    
+    String[] ssthInitialisationAndText = {"sentidata", SENTI_DATA_PATH, "text", textOrFile};
+    
+    do{
+        System.out.println("Explain result? [y/n]");
+        explainStat = in.nextLine();
+    } while(!explainStat.equals("y") || !explainStat.equals("n"));
+    
+    explainStat.equals("y") ? ssthInitialisationAndText[ssthInitialisationAndText.length]= "explain" : ssthInitialisationAndText.add();
+   
+    
     SentiStrength.main(ssthInitialisationAndText); 
 
     //Method 2: One initialisation and repeated classifications
