13546205aceb59ae8f3554b171135f2fb3421d53
Jacob Reed <jacob.reed2017@gmail.com>
2016-10-04 23:46:31 -0500

    added final vars for path vals

--- SentiTool/src/sentitool/SentiTool.java
+++ SentiTool/src/sentitool/SentiTool.java
@@ -12,6 +12,10 @@ import uk.ac.wlv.sentistrength.*;
  */
 public class SentiTool {
 
+    public static final String SENTI_DATA_PATH = "/home/jacob/Development/"
+            + "Undergrad_Research/SentiStrength/SentStrength_Data_Sept2011/"; // String contains the path to the SentiData folder
+    public static String mode; // What mode to run as.  Almost always is text.
+    public static boolean explainStat; // Flag if the user wants to have the results explained or not
     /**
      * @param args the command line arguments
      */
@@ -19,13 +23,13 @@ public class SentiTool {
         // TODO code application logic here
         //Method 1: one-off classification (inefficient for multiple classifications)
     //Create an array of command line parameters, including text or file to process
-    String ssthInitialisationAndText[] = {"sentidata", "/home/jacob/Development/Undergrad_Research/SentiStrength/SentStrength_Data_Sept2011/", "text", "I+hate+frogs+but+love+dogs.", "explain"};
+    String ssthInitialisationAndText[] = {"sentidata", SENTI_DATA_PATH, "text", "I+hate+frogs+but+love+dogs.", "explain"};
     SentiStrength.main(ssthInitialisationAndText); 
 
     //Method 2: One initialisation and repeated classifications
    SentiStrength sentiStrength = new SentiStrength(); 
     //Create an array of command line parameters to send (not text or file to process)
-    String ssthInitialisation[] = {"sentidata", "/home/jacob/Development/Undergrad_Research/SentiStrength/SentStrength_Data_Sept2011/", "explain"};
+    String ssthInitialisation[] = {"sentidata", SENTI_DATA_PATH, "explain"};
     sentiStrength.initialise(ssthInitialisation); //Initialise
     //can now calculate sentiment scores quickly without having to initialise again
     System.out.println(sentiStrength.computeSentimentScores("I hate frogs.")); 
