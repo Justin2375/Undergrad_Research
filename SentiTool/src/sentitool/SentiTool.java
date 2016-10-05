1d2236cedcb4c63c53285518198d7a1a04be491a
Jacob Reed <jacob.reed2017@gmail.com>
2016-10-04 23:50:17 -0500

    new user input

--- SentiTool/src/sentitool/SentiTool.java
+++ SentiTool/src/sentitool/SentiTool.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package sentitool;
+import java.util.Scanner;
 import uk.ac.wlv.sentistrength.*;
 
 /**
@@ -12,18 +13,19 @@ import uk.ac.wlv.sentistrength.*;
  */
 public class SentiTool {
 
-    public static final String SENTI_DATA_PATH = "/home/jacob/Development/"
+    private static final String SENTI_DATA_PATH = "/home/jacob/Development/"
             + "Undergrad_Research/SentiStrength/SentStrength_Data_Sept2011/"; // String contains the path to the SentiData folder
-    public static String mode; // What mode to run as.  Almost always is text.
-    public static boolean explainStat; // Flag if the user wants to have the results explained or not
+    private static String mode; // What mode to run as.  Almost always is text.
+    private static boolean explainStat; // Flag if the user wants to have the results explained or not
+    private Scanner in = new Scanner(System.in); // Scanner for taking in user defined vars
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //Method 1: one-off classification (inefficient for multiple classifications)
+    //Method 1: one-off classification (inefficient for multiple classifications)
     //Create an array of command line parameters, including text or file to process
-    String ssthInitialisationAndText[] = {"sentidata", SENTI_DATA_PATH, "text", "I+hate+frogs+but+love+dogs.", "explain"};
+    String ssthInitialisationAndText[] = {"sentidata", SENTI_DATA_PATH, "text", , "explain"};
     SentiStrength.main(ssthInitialisationAndText); 
 
     //Method 2: One initialisation and repeated classifications
