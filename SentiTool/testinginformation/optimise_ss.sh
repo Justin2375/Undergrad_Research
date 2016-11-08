1fee38e31cea3d5492992b07a7837c66dd3a4c1b
Jacob Reed <jacob.reed2017@gmail.com>
2016-11-07 22:38:11 -0600

    improved sentiment to about 56%

--- SentiTool/testinginformation/optimise_ss.sh
+++ SentiTool/testinginformation/optimise_ss.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar SentiStrengthCom.jar sentidata /home/jacob/Development/Undergrad_Research/SentiTool/data/ minimprovement 2 input /home/jacob/Development/Undergrad_Research/SentiTool/indiv_results.csv  optimise /home/jacob/Development/Undergrad_Research/SentiTool/data/EmotionLookupTable.txt 
+java -jar /home/jacob/Development/Undergraduate_Research/SentiTool/lib/SentiStrengthCom.jar sentidata /home/jacob/Development/Undergrad_Research/SentiTool/data/ minimprovement 2 input /home/jacob/Development/Undergrad_Research/SentiTool/indiv_results.csv  optimise /home/jacob/Development/Undergrad_Research/SentiTool/data/EmotionLookupTable.txt 
