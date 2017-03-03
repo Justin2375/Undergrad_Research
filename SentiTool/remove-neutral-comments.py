31cb5a18c517de97d99f15edecadc6cac8d7ed62
jake32321 <jacob.reed95@siu.edu>
2017-03-02 22:19:35 -0600

    getting the correct comments. next, writing to file

--- SentiTool/remove-neutral-comments.py
+++ SentiTool/remove-neutral-comments.py
@@ -2,24 +2,26 @@
 import csv
 
 #Read the comments from the provided file
-with open("sentiment-oracle-exclude-comments.csv", "r") as excl, open("Sentiment-comments.csv", "r", encoding="ISO-8859-1") as coms:
-    read_one = csv.reader(excl, delimiter=",")
-    read_two = csv.reader(coms, delimiter=",")
+with open("sentiment-oracle-exclude-comments.csv", "r") as excl,open("Sentiment-comments.csv", "r", encoding="ISO-8859-1") as coms:
+    read_one = csv.reader(excl)
+    read_two = csv.reader(coms)
+
+    #Create a list of the remove or not flags
     flag = list(zip(*read_one))[16]
-    comment = list(zip(*read_two))[1]
+    comments_csv = list(read_two)
+
+    index = 0
+    count = 0
+
+    #Loop and check for match 
+    for row in comments_csv:
+        if(index != len(flag)):
+            if(flag[index] != "Ex"):
+                print(row)
+                count += 1
+            index += 1
+
+    print(count)
 
     excl.close()
     coms.close()
-    #excl.close()
-
-index = 0
-count = 0
-
-#Loop and check for match 
-for i in flag:
-    if(i == "Ex"):
-        print(comment[index])
-        count += 1
-    index += 1
-
-print(count)
